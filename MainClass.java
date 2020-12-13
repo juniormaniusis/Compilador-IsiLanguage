56122a68f1143a031fffd4d06de8c107d97db56f
Carlos Maniusis Jr <junior.maniusis@gmail.com>
2020-12-12 23:21:35 -0300

    if gerando codigo java indentado

--- MainClass.java
+++ MainClass.java
@@ -8,8 +8,9 @@ String  t1;
 a= _key.nextDouble();
 b= _key.nextDouble();
 a = 1+2*3/b;
-if ((a+1)>b) {
-System.out.println(a);}else {
+if (((a+1)+2)>b) {
+	System.out.println(a);
+} else {
 System.out.println(b);}
 
 b = a;
